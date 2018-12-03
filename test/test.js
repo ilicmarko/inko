0553f5ac433de75e7b880baae02cf2225cf088c7
Maverick <ilic.marko.05@live.com>
2018-12-03 23:47:16 +0100

    test(helpers): add color validation tests

--- test/test.js
+++ test/test.js
@@ -68,4 +68,60 @@ describe('Path utils', () => {
       expect(helpers.isImage(buffer)).to.be.false;
     });
   })
+
+  describe('Color validation', () => {
+    it('should throw `TypeError` if parameter is not a string', () => {
+      expect(() => helpers.isValidColor(null)).to.throw(TypeError);
+
+      expect(() => helpers.isValidColor(undefined)).to.throw(TypeError);
+
+      expect(() => helpers.isValidColor(Object())).to.throw(TypeError);
+
+      expect(() => helpers.isValidColor(Number())).to.throw(TypeError);
+
+      expect(() => helpers.isValidColor(Boolean())).to.throw(TypeError);
+    });
+    it('should return `true` if color is valid and String object (#RGB)', () => {
+      const hexObject = String('#fff');
+      expect(helpers.isValidColor(hexObject)).to.be.true;
+    });
+    describe('HEX colors', () => {
+      it('should return `true` if color is valid hex (#RGB)', () => {
+        const hex = '#222';
+        expect(helpers.isValidColor(hex)).to.be.true;
+      });
+      it('should return `false` if color is not valid hex (#RGB)', () => {
+        const hex = '#we1';
+        expect(helpers.isValidColor(hex)).to.be.false;
+      });
+      it('should return `true` if color is valid hex (#RRGGBB)', () => {
+        const hex = '#222222';
+        expect(helpers.isValidColor(hex)).to.be.true;
+      });
+      it('should return `false` if color is not valid hex (#RRGGBB)', () => {
+        const hex = '#w31eFp';
+        expect(helpers.isValidColor(hex)).to.be.false;
+      });
+      it('should return `false` if color is not valid hex length', () => {
+        expect(helpers.isValidColor('#22')).to.be.false;
+        expect(helpers.isValidColor('#2222')).to.be.false;
+        expect(helpers.isValidColor('#22222')).to.be.false;
+      });
+      it('should return `true` if color is valid hex case insensitive (#RgB)', () => {
+        const hex = '#FfF';
+        expect(helpers.isValidColor(hex)).to.be.true;
+      });
+    });
+
+    describe('HTML named colors', () => {
+      it('should return `true` if color is valid HTML name', () => {
+        const color = 'rebeccapurple';
+        expect(helpers.isValidColor(color)).to.be.true;
+      });
+      it('should return `true` if color is valid HTML name with case', () => {
+        const color = 'RebeccaPurple';
+        expect(helpers.isValidColor(color)).to.be.true;
+      });
+    })
+  })
 })
